f326a6e9611f92215d6729ae2796ecc535ba209f
Luiz Chequini <luizchequini@gmail.com>
2020-11-12 11:20:04 -0300

    Configurando Nodemon - É muito ruim ter que ficar reiniciando o servidor manualmente a cada alteração no código. Por isso, instalei e configurei o Nodemon dentro do package.json para que ele aplique essas alterações automaticamente e assim ajude na nossa produtividade no ambiente de desenvolvimento.

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({message: 'Hello World'});
+    return response.json({message: 'Bom Dia para todos'});
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+    console.log('🚗🚓🚕Back-end started!🚗🚓🚕');
+});
