c95acc34c8da1ae8023b190d54fa036d0ff8ac03
Luiz Chequini <luizchequini@gmail.com>
2020-11-15 23:33:50 -0300

    Tipos de parâmetros Apliquei na prática os três tipos de parâmetros: Query Params, Route Params e Request Body, e também vi principalmente como enviá-los pelo Insomnia, como configurar o servidor para recebê-los, como utilizá-los no código e quais parâmetros devem ser utilizados em cada rota.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,31 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+/**
+ * Metodos HTTP
+ * 
+ * GET:          Busca informações do back-end
+ * POST:         Criar uma informação no back-end
+ * PUT/PATCH:    Alterar uma informação no back-end
+ * DELETE:       Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ * 
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar e Deletar)
+ * Request Body: Conteúdo na de criar ou editar conteúdo.
+ */
+ 
 app.get('/projects', (request, response) => {
+
+    const {title, owner } = request.query;
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2'
@@ -10,6 +34,12 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+
+    const {title, owner} = request.body;
+
+    console.log(title);
+    console.log(owner);
+
     return response.json([
         'Projeto 1',
         'Projeto 2',
@@ -18,6 +48,11 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+
+    const { id } = request.params;
+
+    console.log(id);
+
     return response.json([
         'Projeto 4',
         'Projeto 2',
