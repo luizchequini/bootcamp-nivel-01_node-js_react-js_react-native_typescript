b80cb6a182a4a6f9b3b1bb252730de7207a4c35d
Luiz Chequini <luizchequini@gmail.com>
2020-11-26 22:47:01 -0300

    Tive que recriar o projeto pois tive problemas com o versionamento do próprio react-native

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,41 +1,27 @@
 import React from 'react';
 import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
-//Não possui valor semântico (significado)
-//Não possui estilização própria
-//Todos compl=onente possuem "display: flex"
-
-//View: div, footer, header, main, aside, section ...
-//Text: p, span, h1, h2, h3 não tem valor semântico
-
-/**
- * Instalações
- * 
- * yarn add axios
- */
-
 export default function App(){
-    return (
-        
+    return(
         <>
-            <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-            <View style={styles.container}>
-                <Text style={styles.title}>Luiz Chequini - Full Stack</Text>
+            <StatusBar barStyle='light-content' backgroundColor='#7159c1'/>
+            <View style={style.container}>
+                <Text style={style.title}>Luiz Chequini - Full Stack</Text>
             </View>
         </>
     );
-}
+};
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
     container:{
-        flex: 1,
+        flex:1,
         backgroundColor:'#7159c1',
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
     },
     title:{
         color:'#fff',
-        fontSize:20,
-        fontWeight:'bold'
-    }
+        fontSize:32,
+        fontWeight:'bold',
+    },
 });
