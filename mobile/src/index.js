5f00da53125239f95bab1d3de674f0b7f9c3be9c
Luiz Chequini <luizchequini@gmail.com>
2020-11-27 17:37:52 -0300

    Nessa aula vamos criar a funcionalidade para criar novos projetos na nossa Lista de Projetos, primeiro vamos ver as alternativas e como criar um botão customizável, depois vamos aprender como chamar um função ao pressionar o botão e para finalizar vamos enviar uma requisição ao Backend para criar o projeto e atualizar a Lista.

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,39 +1,74 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import {SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
 
 import api from './services/api'
+
+/**
+ * adb reverse tcp:3333 tcp:3333
+ */
 
 export default function App(){
     
     const [projects, setProjects] = useState([]);
-    
+
     useEffect(() => {
-        api.get('projects').then(response => {
-            console.log(response.data);
-            setProjects(response.data);
-        })
+      api.get('projects').then(response => {
+        setProjects(response.data);
+      });
     }, []);
 
+
+    async function handlerAddProject(){
+        const response = await api.post('projects', {
+            title: `Projeto ${Date.now()}`,
+            owner: 'Luiz Chequini'
+        });
+
+        const project = projects.data;
+
+        setProjects([...projects, project]);
+    }
 
     return(
         <>
             <StatusBar barStyle='light-content' backgroundColor='#7159c1'/>
-            <View style={style.container}>
-                {projects.map(project => <Text style={style.title} key={project.id} >{project.title}</Text>)}
-            </View>
+            <SafeAreaView style={styles.container}>
+                <FlatList 
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text style={styles.title}>{project.title}</Text>
+                    )}
+                    />
+                
+                <TouchableOpacity activeOpacity={0.5} style={styles.button} onPress={handlerAddProject}>
+                    <Text style={styles.buttonTexto}>Adicionar Projeto</Text>
+                </TouchableOpacity>
+
+            </SafeAreaView>
         </>
     );
 };
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor:'#7159c1',
-        justifyContent:'center',
-        alignItems:'center',
+        backgroundColor:'#7159c1'
     },
     title:{
         color:'#fff',
         fontSize:20
     },
+    button:{
+        backgroundColor:'#fff',
+        margin: 20,
+        height: 50,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonTexto:{
+        fontWeight:'bold',
+        fontSize:16
+    }
 });
